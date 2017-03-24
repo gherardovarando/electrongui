296fc587ff390194e60c937d7db8115e22c74670
gherardovarando <gherardo.varando@gmail.com>
2017-03-24 11:43:29 +0100

    fix err

--- src/ButtonsContainer.js
+++ src/ButtonsContainer.js
@@ -18,7 +18,7 @@
 
 'use strict';
 
-const util = require('util');
+const util = require('./util.js');
 const ToggleElement = require('./ToggleElement');
 
 class ButtonsContainer extends ToggleElement {
