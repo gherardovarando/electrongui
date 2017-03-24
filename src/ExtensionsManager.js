a1d1fa0b9379a5c60df23901d966dd0bdedb162e
gherardovarando <gherardo.varando@gmail.com>
2017-03-24 11:58:02 +0100

    append menu

--- src/ExtensionsManager.js
+++ src/ExtensionsManager.js
@@ -61,6 +61,7 @@ class ExtensionsManager extends GuiExtension {
         this.addMenuItem(new MenuItem({
             type: 'separator'
         }));
+        this.appendMenu();
 
         gui.on('loadextension',(e) =>{
           this.addExtension(e.extension);
